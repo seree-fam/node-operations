fb746109976acf486eb7f2cab8a975720cb9ef82
aibunny <waihenyafredrick@gmail.com>
2025-01-19 10:44:56 +0300

    feat[compose]: entrypoint

--- Dockerfile
+++ Dockerfile
@@ -18,4 +18,4 @@ COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 # Set the entrypoint
-ENTRYPOINT ["/entrypoint.sh"]
+CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,3 @@ if [ ! -f /jwt.hex ]; then
 else
   echo "Using existing jwt.hex."
 fi
-
-# Start the reth node
-exec ./reth/target/release/reth node \
-  --chain sepolia \
-  --datadir /data \
-  --http \
-  --http.addr 0.0.0.0 \
-  --http.port 8545 \
-  --ws \
-  --ws.addr 0.0.0.0 \
-  --ws.port 8546 \
-  --metrics 0.0.0.0:9001 \
-  --authrpc.jwtsecret /jwt.hex \
-  --authrpc.addr 0.0.0.0 \
-  --authrpc.port 8551
