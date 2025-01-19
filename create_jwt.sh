52f858b21291cae3db7e1eae956d42c29a19be0d
aibunny <waihenyafredrick@gmail.com>
2025-01-19 11:00:40 +0300

    feat[sh]: create jwt

--- create_jwt.sh
+++ create_jwt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check if jwt.hex exists, generate if not
-if [ ! -f /jwt.hex ]; then
+if [ ! -f ./jwt.hex ]; then
   echo "Generating jwt.hex..."
   head -c 32 /dev/urandom | hexdump -ve '1/1 "%.2x"' > ./jwt.hex
   echo "jwt.hex generated."
@@ -12,5 +12,5 @@ fi
 #  permissions
 chmod 644 jwt.hex
 
-sudo docker compose up -d --build
+
 
